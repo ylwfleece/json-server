ec521979481ad5f89a98fd5d5f01a7ea707da31f
Max Lettenberger <maxlettenberger@gmail.com>
2022-03-25 16:12:44 -0500

    Add arrow functionality

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
-// get all movies from server
-// generate movie cards 
-// display movie cards
-
-// set up arrows to render based on display indices
-
+let index = 3;
 
 function generateMovieCard(movie) {
-    return `<img id="movie-${movie.id}" src="${movie.imgUrl}" class="moviecarousel__card__img">
-    <p class="moviecarousel__card__name">${movie.name}</p>
-    <p  class="moviecarousel__card__outlineInfo">${movie.outlineInfo}</p>`
+    return `<div class="movie-carousel__card"><img id="movie-${movie.id}" src="${movie.imgUrl}" class="movie-carousel__card__img">
+    <p class="movie-carousel__card__name">${movie.name}</p>
+    <p  class="movie-carousel__card__outlineInfo">${movie.outlineInfo}</p></div>`
+}
+
+function sliceMovies(movies, index) {
+    console.log(index);
+    if (index < 5) {
+        return movies.slice(index, index + 4);
+    } else {
+        return movies.slice(5);
+    }
 }
 
 function generateMovieCarousel(movies) {
-    return movies.map(movie => generateMovieCard(movie)).join('');
+    let fourMovies = sliceMovies(movies, index);
+    return fourMovies.map(movie => generateMovieCard(movie)).join('');
 }
 
 function renderMovieCarousel(movies) {
     const tmp = generateMovieCarousel(movies);
-    const ele = document.querySelector('.moviecarousel');
+    const ele = document.querySelector('.movie-carousel');
     render(ele, tmp);
 }
 
@@ -25,7 +30,38 @@ function render(element, template) {
     element.innerHTML = template;
 }
 
-let movies = fetch('http://localhost:3000/movies')
-    .then(response => response.json())
-    .then(moviesJson => renderMovieCarousel(moviesJson))
+function getMovies() {
+    return fetch('http://localhost:3000/movies')
+        .then((response) => response.json())
+}
+
+function displayMovies() {
+    getMovies().then(movies => {
+        renderMovieCarousel(movies);
+    })
+}
+
+function setUpArrows() {
+
+    document.querySelector('.left-arrow').addEventListener('click', (e) => {
+        index -= 1;
+        displayMovies();
+    })
+
+    document.querySelector('.right-arrow').addEventListener('click', (e) => {
+        index += 1;
+        displayMovies();
+    })
+
+}
+
+function init() {
+   setUpArrows();
+   displayMovies();
+}
+
+init();
+
+
+
     
